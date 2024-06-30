69dbe30ebee845c4f018f379c556c3f7cd1e9ae2
Fahim Mahtab <akashvoratara51@gmail.com>
2024-07-01 00:32:35 +0600

    added new files

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.example.gymmanagement {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.example.gymmanagement to javafx.fxml;
+    exports org.example.gymmanagement;
+}
